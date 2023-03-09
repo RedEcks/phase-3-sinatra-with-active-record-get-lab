c5f6e77cf1ed8ebb65b0794f382215e28760a147
Xavier Dunkley <xavierxv18@gmail.com>
2023-03-09 11:58:41 -0500

    Completed lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
-
   # add routes
+  set :default_content_type, 'application/json'
 
+  get '/bakeries' do
+    bakeries = Bakery.all 
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all
+    sorted_baked_goods = baked_goods.sort_by{|baked_good| -baked_good[:price]}
+    sorted_baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_goods = BakedGood.all
+    most_expensive_good = baked_goods.max_by{|good| good[:price]}
+    most_expensive_good.to_json
+  end
 end
